89f98e5e2d2405f4dcc9d1ac10e2a88278472749
Monika Zamojska <mzamojska@infusion.com>
2015-01-24 12:16:58 +0100

    gallery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
   });
-
+    
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
